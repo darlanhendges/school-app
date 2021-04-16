e78cfb3ee5a11e33ac964e716effdaad5666391f
Darlan <darlanhendges@gmail.com>
2021-04-16 12:02:06 -0300

    feat(app): Feature to force update app

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as  Updates from 'expo-updates';
 
 export default function App() {
+
+  useEffect(() => {
+    async function updateApp() {
+      const { isAvailable } = await Updates.checkForUpdateAsync();
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    }
+    updateApp();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
