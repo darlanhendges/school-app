b56017f38fcdd038ca35109fc85762f28c80ff4a
Dgeison <paradg@gmail.com>
2021-04-19 16:58:30 -0300

    insert splash.png

--- screens/FirstAccess/index.js
+++ screens/FirstAccess/index.js
@@ -1,33 +1,28 @@
-import React from 'react';
-import { View } from 'react-native';
-import { CommonActions } from '@react-navigation/native';
+import React from "react";
+import { View } from "react-native";
+import { CommonActions } from "@react-navigation/native";
 
-
-import { Container, Title, ButtonContainer, TextButton } from './styles';
+import { Container, Title, ButtonContainer, TextButton } from "./styles";
 
 const FirstAccess = ({ navigation }) => {
   return (
-
     <Container>
       <Title>Primeiro Acesso</Title>
 
-      <ButtonContainer onPress={() => {
-        
-        navigation.dispatch(
-          CommonActions.reset({
-            index: 1,
-            routes: [
-              { name: 'Login' },
-              ],
-          })
-        );
-
-      }}>
+      <ButtonContainer
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.reset({
+              index: 1,
+              routes: [{ name: "Login" }],
+            })
+          );
+        }}
+      >
         <TextButton>Ir para login</TextButton>
       </ButtonContainer>
     </Container>
-
   );
-}
+};
 
 export default FirstAccess;
