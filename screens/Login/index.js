541a54f3b35289e9ee0c90be5917b280ae231560
Carlos Eduardo <carloseh.355@gmail.com>
2021-04-20 11:18:10 -0300

    Made some adjustments.

--- screens/Login/index.js
+++ screens/Login/index.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 import {
     KeyboardView,
-    Title,
     Container,
     Brand,
+    Title,
     FullNameInput,
     SignInContainer,
     SignInText,
 } from './styles';
 
-const Login = () => {
-    return (
-        <KeyboardView>
-            <Container>
-                <Brand source={require('../../assets/brand.png')}></Brand>
-                <Title>Comece a aprender um idioma agora.</Title>
-                <FullNameInput placeholder="Nome completo" />
-                
-                <SignInContainer>
-                    <SignInText>ENTRAR</SignInText>
-                </SignInContainer>
-            </Container>
-        </KeyboardView>
-    );
-}
+const Login = () => (
+    <KeyboardView>
+        <Container>
+            <Brand source={require('../../assets/brand.png')}></Brand>
+            <Title>Comece a aprender um idioma agora.</Title>
+            <FullNameInput placeholder="Nome completo" />
+            
+            <SignInContainer>
+                <SignInText>ENTRAR</SignInText>
+            </SignInContainer>
+        </Container>
+    </KeyboardView>
+);
 
 export default Login;
