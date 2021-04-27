d1fc94b1a70e03c2fcbb599c6eb70ec01356ea7d
Darlan Hendges <darlan@labsit.io>
2021-04-27 10:43:28 -0300

    feat(step-apresentatation-screen): Created basic  structure to screen

---
+++ screens/StepApresentation/index.js
@@ -0,0 +1,23 @@
+import React, { } from 'react';
+import AppHeader from '../../components/AppHeader';
+import { ScrollContainer } from './styles';
+
+const StepApresentation = ({ step }) => {
+
+    return (
+        <ScrollContainer>
+            <AppHeader
+                title=""
+                disableBack={false}
+            />
+
+
+
+
+
+
+        </ScrollContainer>
+    )
+}
+
+export default StepApresentation;
