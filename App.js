cdf108a0ebc3bd772c385e1c4c6e57753a55cc41
Darlan Hendges <darlan@labsit.io>
2021-05-05 18:14:21 -0300

    Starting the customization of the fonts from app.

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from "react";
-import * as Updates from "expo-updates";
-import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import React, { useEffect } from 'react';
+import * as Updates from 'expo-updates';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { enableScreens } from 'react-native-screens';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Pacifico_400Regular } from '@expo-google-fonts/pacifico';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { useFonts } from 'expo-font';
+
 import NameContextProvider from './contexts/name';
 import Routes from './routes';
 
 enableScreens();
 
 export default function App() {
-
   useEffect(() => {
     try {
       async function updateApp() {
@@ -24,18 +25,22 @@ export default function App() {
         }
       }
       updateApp();
-    }
-    catch (e) {
+    } catch (e) {
       Alerta('Não foi possível atualizar!');
     }
   }, []);
 
-  let [fontsLoaded] = useFonts({ Pacifico_400Regular, Roboto_400Regular,Roboto_500Medium });
-  if (!fontsLoaded)
-    return <AppLoading />;
+  let [fontsLoaded] = useFonts({
+    Pacifico_400Regular,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Helvetica: require('./assets/fonts/Helvetica.ttf'),
+    Helvetica_Bold: require('./assets/fonts/Helvetica-Bold.ttf'),
+  });
+  if (!fontsLoaded) return <AppLoading />;
 
   return (
-    <SafeAreaProvider style={{flex:1}} >
+    <SafeAreaProvider style={{ flex: 1 }}>
       <NavigationContainer>
         <NameContextProvider>
           <Routes />
--- screens/Login/styles.js
+++ screens/Login/styles.js
@@ -3,47 +3,47 @@ import { COLORS } from '../../constansts/colors';
 import { FONTS } from '../../constansts/fonts';
 
 export const KeyboardView = styled.KeyboardAvoidingView`
-    flex: 1;
+  flex: 1;
 `;
 
 export const Container = styled.View`
-    flex: 1;
-    background-color: ${COLORS.yellow};
-    align-items: center;
-    justify-content: center;
+  flex: 1;
+  background-color: ${COLORS.yellow};
+  align-items: center;
+  justify-content: center;
 `;
 
 export const Brand = styled.Image`
-    width: 55%;
-    height: 8%;
-    margin-bottom: 30px;
+  width: 55%;
+  height: 8%;
+  margin-bottom: 30px;
 `;
 
 export const Title = styled.Text`
-    margin-bottom: 30px;    
-    font-size: 20px;
-    color: ${COLORS.black};
-    font-family: ${FONTS.Pacifico_400Regular};
+  margin-bottom: 30px;
+  font-size: 20px;
+  color: ${COLORS.black};
+  font-family: ${FONTS.Helvetica};
 `;
 
 export const FullNameInput = styled.TextInput`
-    width: 70%;
-    margin-bottom: 15px;
-    padding: 12px 0;
-    text-align: center;
-    font-size: 20px;
-    border: solid ${COLORS.yellowBorderButton} 2px;
-    border-radius: 6px;
+  width: 70%;
+  margin-bottom: 15px;
+  padding: 12px 0;
+  text-align: center;
+  font-size: 20px;
+  border: solid ${COLORS.yellowBorderButton} 2px;
+  border-radius: 6px;
 `;
 
 export const SignInContainer = styled.TouchableOpacity`
-    width: 70%;
-    justify-content: center;
-    margin-bottom: 50px;
+  width: 70%;
+  justify-content: center;
+  margin-bottom: 50px;
 
-    background: ${COLORS.strongGold};
-    border-radius: 6px;
-    padding: 16px 0;
+  background: ${COLORS.strongGold};
+  border-radius: 6px;
+  padding: 16px 0;
 `;
 
 export const SignInText = styled.Text`
