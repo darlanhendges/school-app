cb8c922ddab4cb9f6b882743b92378989e162910
Darlan Hendges <darlan@labsit.io>
2021-05-07 10:58:37 -0300

    Included audio component in Step Apresentation.

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as ActivityIndicator } from './ActivityIndicator';
 export { default as MainButton } from './MainButton';
 export { default as InfoModal } from './InfoModal';
+export { default as Video } from './Video';
--- contexts/steps.js
+++ contexts/steps.js
@@ -1,45 +1,46 @@
 import React, { createContext, useState } from 'react';
 import StepService from '../services/StepService';
 
-export const StepsContext = createContext({ steps: '', setSteps: null, getSteps: null });
-
+export const StepsContext = createContext({
+  steps: '',
+  setSteps: null,
+  getSteps: null,
+});
 
 export default ({ children }) => {
-    const [steps, setSteps] = useState([]);
+  const [steps, setSteps] = useState([]);
 
-    async function getSteps() {
+  async function getSteps() {
+    const steps = await StepService.getSteps();
+    let stepsBuilt = [];
 
-        const steps = await StepService.getSteps();
-        let stepsBuilt = [];
+    steps.map((item) => {
+      const { id, data } = item;
+      console.log(data);
 
-        steps.map((item) => {
-            const { id, data } = item;
-            
+      try {
+        let itemToPush = {
+          id,
+          title: data.title ? data.title[0].text : '',
+          subtitle: data.subtitle[0].text,
+          featured_image: data.featured_image.url,
+          step_presentation: data.step_presentation[0].text,
+          presentation_image: data.presentation_image.url,
+          presentation_text: data.presentation_text[0].text,
+          presentation_youtube: data.presentation_youtube,
+          step_completion_message: data.step_completion_message[0].text,
+        };
+        stepsBuilt.push(itemToPush);
+        setSteps(stepsBuilt);
+      } catch (e) {
+        alert('Não foi possível montar a etapa: ' + title);
+      }
+    });
+  }
 
-            try {
-
-                let itemToPush = {
-                    id,
-                    title: data.title[0].text,
-                    subtitle: data.subtitle[0].text,
-                    featured_image: data.featured_image.url,
-                    step_presentation: data.step_presentation[0].text,
-                    presentation_image: data.presentation_image.url,
-                    presentation_text: data.presentation_text[0].text,
-                    step_completion_message: data.step_completion_message[0].text
-                };
-                stepsBuilt.push(itemToPush);
-                setSteps(stepsBuilt);
-            }
-            catch (e) {
-                alert('Não foi possível montar a etapa: ' + title);
-            }
-        });
-    }
-
-    return (
-        <StepsContext.Provider value={{ steps, setSteps, getSteps }}>
-            {children}
-        </StepsContext.Provider>
-    );
+  return (
+    <StepsContext.Provider value={{ steps, setSteps, getSteps }}>
+      {children}
+    </StepsContext.Provider>
+  );
 };
